443484d76b7ef292acfbc7effa1ed144e1cdded8
sriharsha <harsha.471@gmail.com>
2022-06-22 13:07:41 +0530

    Initial commit

---
+++ src/main/java/com/codeusingjava/vault/configuration/VaultConfig.java
@@ -0,0 +1,30 @@
+package com.codeusingjava.vault.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VaultConfig {
+    @Value("${dbusername}")
+	private String username;
+
+    @Value("${dbpassword}")
+    public String password;
+
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
